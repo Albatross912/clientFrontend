5a8095efc933d60f3c7e61cce5c396d3298a7338
Albatross912 <piyushsharma912912@gmail.com>
2024-04-20 12:01:16 +0530

    remove footer

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
         <QueryClientProvider client={queryClient}>
           <Navbar />
           <Outlet />
-          <Footer />
+          {/* <Footer /> */}
         </QueryClientProvider>
       </div>
     );
